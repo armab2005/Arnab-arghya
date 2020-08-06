6a42d06c61acb49cf87a19908f48ed814e06b04d
armab2005 <69154178+armab2005@users.noreply.github.com>
2020-08-06 12:37:40 +0530

    Update Amspeak.py

--- Amspeak.py
+++ Amspeak.py
@@ -1,3 +1,7 @@
 #A python based project
 #============Let us start ========================
 from textblob import TextBlob 
+text=input('Please submit your feelings here ')
+sentiment=TextBlob(text). sentiment.polarity
+print (sentiment)
+#=========If negative he/she is sad and if positive he /she is Happy=========
